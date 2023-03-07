7b28ce9c8253ab022259ab16dcfd636f0291028d
gnehs <jayuiop@gmail.com>
2023-03-08 01:05:19 +0800

    updated style

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -66,7 +66,7 @@ async function createWindow() {
 
   // Test actively push message to the Electron-Renderer
   win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', new Date().toLocaleString())
+    win?.webContents.send('main-process-message', ['version', app.getVersion()])
   })
 
   // Make all links open with the browser, not with the application
--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,23 +1,27 @@
 import Translator from "@/components/translator"
+import { ipcRenderer } from 'electron';
+import { useEffect, useState } from 'react';
 function DefaultLayout() {
+  const [version, setVersion] = useState<string | null>(null)
+  useEffect(() => {
+    ipcRenderer.on('main-process-message', (_event, args) => {
+      console.log('[Receive Main-process message]:', args);
+      if (args[0] == 'version') {
+        setVersion(args[1])
+      }
+    });
+  }, [])
   return (
     <div className="default-layout">
-      <div className="navbar">
-        <div className="logo">
-          <i className='bx bx-transfer-alt'></i> Subtitle translator
-        </div>
-        <div className="links">
-          <a href="https://github.com/gnehs/subtitle-translator-electron" target="_blank">
-            <i className='bx bxl-github'></i>
-          </a>
-        </div>
-      </div>
       <Translator className="content" />
       <div className="footer">
         <div className="content">
-          Developed by <a href="https://gnehs.net/" target="_blank">gnehs</a> | Made with <a href="https://pancake.tw/" target="_blank">🥞</a> in Taiwan
+          v{version} | Developed by <a href="https://gnehs.net/" target="_blank">gnehs</a> | Made with <a href="https://pancake.tw/" target="_blank">🥞</a> in Taiwan
         </div>
         <div className="links">
+          <a href="https://github.com/gnehs/subtitle-translator-electron" target="_blank">
+            <i className='bx bxl-github'></i> GitHub
+          </a>
           <a href="https://github.com/gnehs/subtitle-translator-electron/issues" target="_blank">
             <i className='bx bx-bug' ></i> Report a bug
           </a>
