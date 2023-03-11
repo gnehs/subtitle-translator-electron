5d660feee7309c5b5522f8ea95a55fb24d06bd91
gnehs <jayuiop@gmail.com>
2023-03-11 20:21:40 +0800

    auto check new version

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -3,6 +3,7 @@ import { ipcRenderer } from 'electron';
 import { useEffect, useState } from 'react';
 function DefaultLayout() {
   const [version, setVersion] = useState<string | null>(null)
+  const [newVersion, setNewVersion] = useState<string | null>(null)
   useEffect(() => {
     ipcRenderer.on('main-process-message', (_event, args) => {
       console.log('[Receive Main-process message]:', args);
@@ -13,13 +14,25 @@ function DefaultLayout() {
         document.documentElement.style.setProperty('--accent-color', `#${args[1]}`)
       }
     });
+    fetch('https://api.github.com/repos/gnehs/subtitle-translator-electron/releases/latest').then(res => res.json()).then(res => {
+      setNewVersion(res.tag_name)
+    })
   }, [])
   return (
     <div className="default-layout">
       <Translator className="content" />
       <div className="footer">
         <div className="content">
-          v{version} | Developed by <a href="https://gnehs.net/" target="_blank">gnehs</a> | Made with <a href="https://pancake.tw/" target="_blank">🥞</a> in Taiwan
+          {(version && newVersion && version != newVersion) ?
+            <a className="new-version" href="https://github.com/gnehs/subtitle-translator-electron/releases" target="_blank">
+              <i className='bx bx-up-arrow-alt' ></i> New version ({version}<i className='bx bx-right-arrow-alt' ></i>{newVersion})
+            </a> :
+            <span>{version}</span>
+          }
+          <span> | </span>
+          <span>Developed by <a href="https://gnehs.net/" target="_blank">gnehs</a></span>
+          <span> | </span>
+          <span>Made with <a href="https://pancake.tw/" target="_blank">🥞</a> in Taiwan</span>
         </div>
         <div className="links">
           <a href="https://www.buymeacoffee.com/gnehs" target="_blank">
