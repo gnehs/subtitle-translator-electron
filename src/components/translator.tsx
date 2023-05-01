e847817f507f3b1829b926d0e38f0d95a6b7c661
Limon Monte <6059356+limonte@users.noreply.github.com>
2023-05-01 11:26:42 +0300

    chore: you don't have to be polite with computers

--- src/components/translator.tsx
+++ src/components/translator.tsx
@@ -114,7 +114,7 @@ function Translator({ className }: { className?: string }) {
       messages: [
         {
           role: "system",
-          content: `You are a program responsible for translating subtitles. Your task is to output the specified target language based on the input text. Please do not create the following subtitles on your own. Please do not output any text other than the translation. You will receive the subtitles as array that needs to be translated, as well as the previous translation results and next subtitle. Please transliterate the person's name into the local language. Target language: ${targetLanguage}\n\n${additionalNotes}`
+          content: `You are a program responsible for translating subtitles. Your task is to output the specified target language based on the input text. Do not create the following subtitles on your own. Do not output any text other than the translation. You will receive the subtitles as array that needs to be translated, as well as the previous translation results and next subtitle. Transliterate the person's name into the local language. Target language: ${targetLanguage}\n\n${additionalNotes}`
         },
         {
           role: "user",
@@ -193,7 +193,7 @@ function Translator({ className }: { className?: string }) {
         messages: [
           {
             role: "system",
-            content: `You are a program responsible for translating subtitles. Your task is to output the specified target language based on the input text. Please do not create the following subtitles on your own. Please do not output any text other than the translation. You will receive the subtitles as array that needs to be translated, as well as the previous translation results and next subtitle. If you need to merge the subtitles with the following line, simply repeat the translation. Please transliterate the person's name into the local language. Target language: ${targetLanguage}\n\n${additionalNotes}`
+            content: `You are a program responsible for translating subtitles. Your task is to output the specified target language based on the input text. Do not create the following subtitles on your own. Do not output any text other than the translation. You will receive the subtitles as array that needs to be translated, as well as the previous translation results and next subtitle. If you need to merge the subtitles with the following line, simply repeat the translation. Transliterate the person's name into the local language. Target language: ${targetLanguage}\n\n${additionalNotes}`
           },
           ...previousSubtitles.slice(-4),
           {
