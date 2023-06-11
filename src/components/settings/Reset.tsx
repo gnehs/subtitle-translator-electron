57c0414e74e4d6231b82cecf6f29dbd61829419f
gnehs <jayuiop@gmail.com>
2023-06-11 22:00:40 +0800

    updated reset style

--- src/components/settings/Reset.tsx
+++ src/components/settings/Reset.tsx
@@ -12,7 +12,7 @@ export default function Reset() {
     <div className="flex flex-col gap-2">
       <Title>{t(`reset.title`)}</Title>
       <button
-        className={`p-2 rounded cursor-pointer bg-red-100 hover:bg-red-200 active:bg-red-300`}
+        className={`p-2 rounded cursor-pointer bg-red-100 hover:bg-red-200 active:bg-red-300 text-red-500 font-bold`}
         onClick={(e) => resetAll()}
       >
         {t(`reset.name`)}
