04b93b81533996053b8ba0132c85c520c4aca0d0
gnehs <jayuiop@gmail.com>
2023-06-11 18:12:27 +0800

    added model settings

---
+++ src/components/settings/Model.tsx
@@ -0,0 +1,31 @@
+import Title from "../Title";
+import { useTranslation } from "react-i18next";
+import { useLocalStorage } from "usehooks-ts";
+export default function Language() {
+  const { t, i18n } = useTranslation();
+  const [model, setModel] = useLocalStorage("model", "gpt-3.5-turbo");
+  const model_list = [`gpt-4-0314`, `gpt-3.5-turbo`, `gpt-3.5-turbo-economy`];
+  return (
+    <>
+      <Title>{t("model")}</Title>
+      <div className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-2">
+        {model_list.map((id) => (
+          <button
+            key={id}
+            className={`p-2 rounded cursor-pointer text-left flex flex-col ${
+              id == model
+                ? `bg-slate-300 font-bold`
+                : `bg-slate-100 hover:bg-slate-200 active:bg-slate-300`
+            }}`}
+            onClick={() => setModel(id)}
+          >
+            <div className={id == model ? `font-bold` : ``}>
+              {t(`models.${id}.name`)}
+            </div>
+            <div className="opacity-80">{t(`models.${id}.description`)}</div>
+          </button>
+        ))}
+      </div>
+    </>
+  );
+}
--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import Language from "@/components/settings/Language";
+import Model from "@/components/settings/Model";
 import { useTranslation } from "react-i18next";
 export default function Settings() {
   const { t, i18n } = useTranslation();
@@ -8,6 +9,7 @@ export default function Settings() {
       <Header>{t(`settings`)}</Header>
       <div className="p-2 flex flex-col gap-2">
         <Language />
+        <Model />
         <h1 className="font-bold text-slate-800">API Key & Host</h1>
         <h1 className="font-bold text-slate-800">Prompt</h1>
       </div>
