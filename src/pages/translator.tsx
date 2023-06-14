eb2618e18bd998e8b218c75cecb33cb4e43ca8d7
gnehs <jayuiop@gmail.com>
2023-06-14 23:55:12 +0800

    removed test step viewer

--- src/pages/translator.tsx
+++ src/pages/translator.tsx
@@ -1,18 +1,13 @@
 import Header from "@/components/Header";
-import InputField from "@/components/InputField";
-import TextareaField from "@/components/TextareaField";
-import { useLocalStorage } from "usehooks-ts";
-import { useTranslation } from "react-i18next";
 import File from "@/components/translator/File";
 import Lang from "@/components/translator/Lang";
 import Preview from "@/components/translator/Preview";
 
 import useStep from "@/hooks/useStep";
 export default function Translator() {
-  const [step] = useStep();
   return (
     <div className="flex flex-col h-full">
-      <Header>Subtitle Translator {step}</Header>
+      <Header>Subtitle Translator</Header>
       <div className="flex flex-row h-full items-center justify-center">
         <File />
         <Lang />
