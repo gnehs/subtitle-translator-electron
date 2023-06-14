a2688192bf44b860e8764ae3a6b432b103602e4c
gnehs <jayuiop@gmail.com>
2023-06-14 23:55:31 +0800

    added previousStep

--- src/components/translator/Preview.tsx
+++ src/components/translator/Preview.tsx
@@ -1,12 +1,13 @@
 import Button from "../Button";
 import useStep from "../../hooks/useStep";
 export default function File() {
-  const [step, nextStep] = useStep();
+  const [step, nextStep, previousStep] = useStep();
   if (step !== 3) return null;
   return (
     <>
       <div className="flex flex-col gap-2 h-full">
         <div className="flex flex-row h-full">Preview</div>
+        <Button onClick={() => previousStep()}>previousStep</Button>
         <Button onClick={() => nextStep()}>Next</Button>
       </div>
     </>
