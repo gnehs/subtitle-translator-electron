e856426f0b4e86a655ddf9904af03e64471ca1f2
gnehs <jayuiop@gmail.com>
2023-06-15 00:13:10 +0800

    updated useFile

--- src/hooks/useFile.ts
+++ src/hooks/useFile.ts
@@ -1,9 +1,33 @@
 import { useSelector, useDispatch } from "react-redux";
 import { setFile } from "@/store/file";
+import { useState, useEffect } from "react";
 export default function useStep() {
+  // Get
   //@ts-ignore
-  const file = useSelector((state) => state.file.value);
+  const fileInfo = useSelector((state) => state.file.value);
+  const [file, setFileState] = useState(null);
+  useEffect(() => {
+    if (fileInfo) {
+      fetch(fileInfo.url)
+        .then((r) => r.blob())
+        .then(
+          (blobFile) =>
+            new File([blobFile], fileInfo.name, { type: fileInfo.type })
+        )
+        .then((file) => {
+          //@ts-ignore
+          setFileState(file);
+        });
+    }
+  }, [fileInfo]);
+
+  // Set
   const dispatch = useDispatch();
-  const _setFile = (file: any) => dispatch(setFile(file));
+  const _setFile = (file: any) => {
+    let fileURL = URL.createObjectURL(file);
+    dispatch(setFile({ url: fileURL, type: file.type, name: file.name }));
+  };
+
+  // Return
   return [file, _setFile];
 }
