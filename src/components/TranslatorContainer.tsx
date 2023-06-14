1ceccb7453d18f65b3f3b043e68c5bd9dd660060
gnehs <jayuiop@gmail.com>
2023-06-14 23:45:32 +0800

    added TranslatorContainer

---
+++ src/components/TranslatorContainer.tsx
@@ -0,0 +1,38 @@
+import Button from "./Button";
+import { useTranslation } from "react-i18next";
+import useStep from "../hooks/useStep";
+import Title from "./Title";
+export default function TranslatorContainer({
+  children,
+  title,
+}: {
+  children: React.ReactNode;
+  title: string;
+}) {
+  const { t } = useTranslation();
+  const [step, nextStep, previousStep] = useStep();
+  return (
+    <form
+      className="flex flex-col gap-2 items-center justify-center w-full max-w-[368px] mx-auto bg-slate-50 border border-slate-300 rounded"
+      onSubmit={(e) => {
+        e.preventDefault();
+        nextStep();
+      }}
+    >
+      <div className="p-2 bg-slate-200 w-full rounded-t">
+        <Title>{title}</Title>
+      </div>
+      <div className="w-full px-2 flex flex-col gap-2">{children}</div>
+      <div className="p-2 w-full flex gap-2">
+        {step == 2 && (
+          <Button className="w-full shadow" onClick={() => previousStep()}>
+            Previous
+          </Button>
+        )}
+        <Button submit className="w-full shadow">
+          Next
+        </Button>
+      </div>
+    </form>
+  );
+}
--- src/components/translator/Lang.tsx
+++ src/components/translator/Lang.tsx
@@ -1,10 +1,9 @@
-import Button from "../Button";
 import InputField from "@/components/InputField";
 import TextareaField from "@/components/TextareaField";
 import { useLocalStorage } from "usehooks-ts";
 import { useTranslation } from "react-i18next";
 import useStep from "../../hooks/useStep";
-import Title from "../Title";
+import TranslatorContainer from "../TranslatorContainer";
 export default function File() {
   const { t } = useTranslation();
   const [lang, setLang] = useLocalStorage("translate_lang", "");
@@ -13,31 +12,26 @@ export default function File() {
     ""
   );
 
-  const [step, nextStep] = useStep();
+  const [step] = useStep();
   if (step !== 1) return null;
   return (
     <>
-      <div className="flex flex-col gap-2 items-center justify-center w-full max-w-[368px] mx-auto bg-slate-50 p-2 rounded">
-        <Title>{t(`translate.language`)}</Title>
-        <div className="w-full p-2 flex flex-col gap-2">
-          <InputField
-            label={t(`translate.target`)}
-            placeholder={t(`translate.target_description`)!}
-            value={lang}
-            onChange={(e: any) => setLang(e.target.value)}
-          />
-          <TextareaField
-            label={t(`translate.additional`)}
-            placeholder={t(`translate.additional_description`)!}
-            value={additional}
-            onChange={(e: any) => setAdditional(e.target.value)}
-            minHeight="200px"
-          />
-        </div>
-        <Button className="w-full" onClick={() => nextStep()}>
-          Next
-        </Button>
-      </div>
+      <TranslatorContainer title={t(`translate.language`)}>
+        <InputField
+          label={t(`translate.target`)}
+          placeholder={t(`translate.target_description`)!}
+          value={lang}
+          onChange={(e: any) => setLang(e.target.value)}
+          required
+        />
+        <TextareaField
+          label={t(`translate.additional`)}
+          placeholder={t(`translate.additional_description`)!}
+          value={additional}
+          onChange={(e: any) => setAdditional(e.target.value)}
+          minHeight="200px"
+        />
+      </TranslatorContainer>
     </>
   );
 }
