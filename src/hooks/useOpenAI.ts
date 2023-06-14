257bdc8be0104a131b153c4f43f11f453351cf52
gnehs <jayuiop@gmail.com>
2023-06-15 02:34:46 +0800

    updated useOpenAI

--- src/hooks/useOpenAI.ts
+++ src/hooks/useOpenAI.ts
@@ -1,22 +1,48 @@
 import { Configuration, OpenAIApi } from "openai";
 import { useLocalStorage } from "usehooks-ts";
-export function createChatCompletion() {
+import usePrompt from "./usePrompt";
+import useModel from "./useModel";
+export function translate() {
   const [apiKeys] = useAPIKeys();
   const [apiHost] = useAPIHost();
+  const [model] = useModel();
+  const [prompt] = usePrompt(model);
+  const [lang] = useLocalStorage("translate_lang", "");
+  const [additional] = useLocalStorage("translate_additional", "");
+
   //@ts-ignore
   let openAIInstance = [];
   for (let apiKey of apiKeys) {
     const configuration = new Configuration({ apiKey, basePath: apiHost });
     const openai = new OpenAIApi(configuration);
-    openai.createChatCompletion;
     openAIInstance.push(openai);
   }
-  //@ts-ignore
-  return function createChatCompletion(i, ...args) {
+  const getRandomInstance = () => {
     //@ts-ignore
-    return openAIInstance[i % openAIInstance.length].createChatCompletion(
-      ...args
-    );
+    return openAIInstance[
+      Math.floor(Math.random() * openAIInstance.length)
+    ] as OpenAIApi;
+  };
+
+  //@ts-ignore
+  return async function translate(i: number, subtitles: array[string]) {
+    let { createChatCompletion } = getRandomInstance();
+    let presedPrompt = prompt
+      .replace("{{lang}}", lang)
+      .replace("{{additional}}", additional);
+    return await createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: presedPrompt,
+        },
+        {
+          role: "user",
+          content: JSON.stringify(subtitles),
+        },
+      ],
+    });
   };
 }
 export function useAPIKeys() {
