a0d3d2c18cd11d072e3529066e0d6197034ae7fd
gnehs <jayuiop@gmail.com>
2023-06-15 22:41:15 +0800

    removed unused import

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,7 +2,6 @@ import Header from "@/components/Header";
 import Button from "@/components/Button";
 import { useVersion } from "@/hooks/useVersion";
 import { useTranslation } from "react-i18next";
-import { motion } from "framer-motion";
 import icon from "@/assets/icon.png";
 import BuyMeACoffee from "@/components/BuyMeACoffee";
 export default function About() {
