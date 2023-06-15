dab98110d49153a15ef28f478b8190c565c21280
gnehs <jayuiop@gmail.com>
2023-06-15 20:29:18 +0800

    add zh-CN & updated locale

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,6 +1,8 @@
 import en_US from "./en-US.json";
 import zh_TW from "./zh-TW.json";
+import zh_CN from "./zh-CN.json";
 export default {
   "en-US": en_US,
   "zh-TW": zh_TW,
+  "zh-CN": zh_CN,
 };
