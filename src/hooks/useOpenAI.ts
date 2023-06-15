7f68e230df24f4530da110d9055387089423d259
gnehs <jayuiop@gmail.com>
2023-06-15 15:35:00 +0800

    added translateSubtitleSingle

--- src/hooks/useOpenAI.ts
+++ src/hooks/useOpenAI.ts
@@ -20,49 +20,94 @@ export function useTranslate() {
   }
 
   //@ts-ignore
-  return async function translate(subtitles: array[]) {
-    let ai = openAIInstance[Math.floor(Math.random() * openAIInstance.length)];
-    let presedPrompt = prompt
-      .replace("{{lang}}", lang)
-      .replace("{{additional}}", additional);
-    let modelName = {
-      "gpt-4": "gpt-4-0613",
-      "gpt-3.5-turbo": "gpt-3.5-turbo-0613",
-      "gpt-3.5-turbo-economy": "gpt-3.5-turbo-0613",
-    }[model]!;
-    return await ai.createChatCompletion({
-      model: modelName,
-      messages: [
-        {
-          role: "system",
-          content: presedPrompt,
-        },
-        {
-          role: "user",
-          content: JSON.stringify(subtitles),
-        },
-      ],
-      functions: [
-        {
-          name: "setResult",
-          description: "Sets the result of the translation",
-          parameters: {
-            type: "object",
-            properties: {
-              result: {
-                type: "array",
-                description: "The translated subtitles",
-                items: {
-                  type: "string",
-                  description: "A subtitle",
+  return {
+    translateSubtitleChunk: async function (subtitles: Array<any>) {
+      let ai =
+        openAIInstance[Math.floor(Math.random() * openAIInstance.length)];
+      let presedPrompt = prompt
+        .replace("{{lang}}", lang)
+        .replace("{{additional}}", additional);
+      let modelName = {
+        "gpt-4": "gpt-4-0613",
+        "gpt-3.5-turbo": "gpt-3.5-turbo-0613",
+        "gpt-3.5-turbo-economy": "gpt-3.5-turbo-0613",
+      }[model]!;
+      return await ai.createChatCompletion({
+        model: modelName,
+        messages: [
+          {
+            role: "system",
+            content: presedPrompt,
+          },
+          {
+            role: "user",
+            content: JSON.stringify(subtitles),
+          },
+        ],
+        functions: [
+          {
+            name: "setResult",
+            description: "Sets the result of the translation",
+            parameters: {
+              type: "object",
+              properties: {
+                result: {
+                  type: "array",
+                  description: "The translated subtitles",
+                  items: {
+                    type: "string",
+                    description: "A subtitle",
+                  },
                 },
               },
+              required: ["result"],
             },
-            required: ["result"],
           },
-        },
-      ],
-    });
+        ],
+      });
+    },
+    translateSubtitleSingle: async function (subtitle: string) {
+      let ai =
+        openAIInstance[Math.floor(Math.random() * openAIInstance.length)];
+      let presedPrompt =
+        `prompt: You are a program responsible for translating subtitles. Your task is to output the specified target language based on the input text. Please do not create the following subtitles on your own. Use setResult function to return the result. Target language: {{lang}}\n\n{{additional}}`
+          .replace("{{lang}}", lang)
+          .replace("{{additional}}", additional);
+      let modelName = {
+        "gpt-4": "gpt-4-0613",
+        "gpt-3.5-turbo": "gpt-3.5-turbo-0613",
+        "gpt-3.5-turbo-economy": "gpt-3.5-turbo-0613",
+      }[model]!;
+      return await ai.createChatCompletion({
+        model: modelName,
+        messages: [
+          {
+            role: "system",
+            content: presedPrompt,
+          },
+          {
+            role: "user",
+            content: subtitle,
+          },
+        ],
+        functions: [
+          {
+            name: "setResult",
+            description: "Sets the result of the translation",
+            parameters: {
+              type: "object",
+              properties: {
+                result: {
+                  type: "string",
+                  description: "The translated subtitle",
+                },
+              },
+              required: ["result"],
+            },
+          },
+        ],
+      });
+    },
   };
 }
 export function useAPIKeys() {
